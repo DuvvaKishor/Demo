fbd5fb7074815d283d7d69be657a31536699d0ee
Vivaansh <vivaansh@Vivaansh.lan>
2025-01-04 15:12:06 +0530

    Updates

---
+++ precache-manifest.9990d5a984e8d4a29048d8516db1c31c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf8768a320dcb8f47cc5a8509b8d414e",
+    "url": "/Demo/index.html"
+  },
+  {
+    "revision": "54f0a2a871c5d7e812b8",
+    "url": "/Demo/static/css/main.af1b4155.chunk.css"
+  },
+  {
+    "revision": "039b039f6a6eff0178e7",
+    "url": "/Demo/static/js/2.263563ec.chunk.js"
+  },
+  {
+    "revision": "3b860c6f7226e33f8282",
+    "url": "/Demo/static/js/3.290014af.chunk.js"
+  },
+  {
+    "revision": "54f0a2a871c5d7e812b8",
+    "url": "/Demo/static/js/main.5f2b0686.chunk.js"
+  },
+  {
+    "revision": "bed5ea40b464695ab3c6",
+    "url": "/Demo/static/js/runtime~main.3363057d.js"
+  }
+]);
